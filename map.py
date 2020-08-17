de10554d81f0f7c37432556799ccaff00fea35d3
instigation <instigation@naver.com>
2020-08-17 23:05:18 +0900

    Refactored Map class

--- map.py
+++ map.py
@@ -3,15 +3,15 @@ from geometry import OpenSquare
 
 class Map:
     def __init__(self, food_num, rand_pos_generator, map_width, map_height):
-        self.next_food_id_to_issue = 0
+        assert ((food_num > 0) and (rand_pos_generator is not None)) or ((food_num == 0) and (rand_pos_generator is None))
         self.rand_pos_generator = rand_pos_generator
+        self.next_food_id_to_issue = 0
         if rand_pos_generator is not None:
             self.food_positions = [rand_pos_generator(n) for n in range(food_num)]
             self.next_food_id_to_issue = food_num
         else:
             self.food_positions = []
-        self.map_width = map_width
-        self.map_height = map_height
+        self.map_area = OpenSquare(map_width, map_height)
 
     def eat_food_at(self, position):
         self.remove_food_at(position)
@@ -28,5 +28,4 @@ class Map:
         return self.food_positions
 
     def not_includes(self, snake):
-        map_area = OpenSquare(self.map_width, self.map_height)
-        return not map_area.includes(snake.get_head())
+        return not self.map_area.includes(snake.get_head())
